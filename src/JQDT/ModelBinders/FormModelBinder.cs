9157edc08fba32002933d5afc7682e04f484c8a3
vladko_sz <vdimov1124@gmail.com>
2017-11-16 12:17:56 +0200

    Validation to model binder arguments added

--- src/JQDT/ModelBinders/FormModelBinder.cs
+++ src/JQDT/ModelBinders/FormModelBinder.cs
@@ -4,6 +4,7 @@
     using System.Collections.Generic;
     using System.Collections.Specialized;
     using System.Linq;
+    using System.Reflection;
     using System.Text.RegularExpressions;
     using JQDT.Models;
 
@@ -12,6 +13,25 @@
     /// </summary>
     internal class FormModelBinder
     {
+        private const string NullAjaxFormException = "Ajax form cannot be null.";
+        private const string EmptyAjaxForm = "Ajax form cannot be empty.";
+
+        private const string LengthFormKey = "length";
+        private const string StartFormKey = "start";
+        private const string SearchValueFormKey = "search[value]";
+        private const string CustomFiltersPatternFormKey = @"^custom\[filters\]\[(.+)\]\[(gte|gt|lte|lt)\]$";
+        private const string ColumnsOrderableFormKey = "columns[{0}][orderable]";
+        private const string ColumnsSearchableFormKey = "columns[{0}][searchable]";
+
+        private const string ColumnsFormKeyPattern = @"^columns\[(\d+)\]\[data\]$";
+        private const string ColumnsSearchValueFormKeyPattern = "columns[{0}][search][value]";
+        private const string ColumnsSearchRegexFormKeyPattern = "columns[{0}][search][regex]";
+        private const string OrderDirectionFormKeyPattern = @"^order\[(\d+)\]\[dir\]$";
+
+        private const string MissingMandatoryKeyException = "The form value \"{0}\" is mandatory but is missing in the ajax request content.";
+        private const string NullDataException = "The data collection cannot be null.";
+        private const string InvalidDataTypeException = "The data must be a valid collection. The provided data is of type {0}";
+
         private Dictionary<string, string> ajaxFormDictionary;
 
         /// <summary>
@@ -22,16 +42,29 @@
         /// <returns><see cref="RequestInfoModel"/></returns>
         public RequestInfoModel BindModel(NameValueCollection ajaxForm, object data)
         {
+            if (ajaxForm == null)
+            {
+                throw new ArgumentNullException(NullAjaxFormException);
+            }
+
+            if (ajaxForm.Count == 0)
+            {
+                throw new ArgumentException(EmptyAjaxForm);
+            }
+
+            if (data == null)
+            {
+                throw new ArgumentNullException(NullDataException);
+            }
+
             this.InitializeAjaxFormDictionary(ajaxForm);
+            this.ValidateMandatoryValues(LengthFormKey, StartFormKey);
 
-            var lengthStr = this.ajaxFormDictionary["length"];
-
-            // TODO: Throw appropriate exceptions when mandatory value is missing;
             int start = 0;
-            int.TryParse(this.ajaxFormDictionary["start"], out start);
+            int.TryParse(this.ajaxFormDictionary[StartFormKey], out start);
 
             int length = 0;
-            int.TryParse(this.ajaxFormDictionary["length"], out length);
+            int.TryParse(this.ajaxFormDictionary[LengthFormKey], out length);
 
             var datatableModel = new DataTableAjaxPostModel
             {
@@ -39,7 +72,7 @@
                 Length = length,
                 Search = new Search
                 {
-                    Value = this.ajaxFormDictionary["search[value]"]
+                    Value = this.GetSearchValue()
                 },
                 Order = this.GetOrderList(),
                 Columns = this.GetColumns(),
@@ -51,11 +84,48 @@
                 TableParameters = datatableModel,
                 Helpers = new RequestHelpers
                 {
-                    ModelType = data.GetType().GenericTypeArguments.First()
+                    ModelType = this.GetModelType(data)
                 }
             };
 
             return requestInfoModel;
+        }
+
+        private Type GetModelType(object data)
+        {
+            var dataType = data.GetType();
+            if (((TypeInfo)dataType).ImplementedInterfaces.Any(t => t == typeof(IEnumerable<>)))
+            {
+                throw new ArgumentException(string.Format(InvalidDataTypeException, dataType.FullName));
+            }
+
+            var modelType = dataType.GenericTypeArguments.First();
+
+            return modelType;
+        }
+
+        /// <summary>
+        /// Safely attempt to get the search value. If the value is missing in the ajax request content
+        /// an empty string is returned.
+        /// </summary>
+        /// <returns>The search value or empty string if the value is missing.</returns>
+        private string GetSearchValue()
+        {
+            string searchValue = string.Empty;
+            this.ajaxFormDictionary.TryGetValue(SearchValueFormKey, out searchValue);
+
+            return searchValue;
+        }
+
+        private void ValidateMandatoryValues(params string[] mandatoryKeys)
+        {
+            foreach (var key in mandatoryKeys)
+            {
+                if (!this.ajaxFormDictionary.ContainsKey(key))
+                {
+                    throw new ArgumentException(string.Format(MissingMandatoryKeyException, key));
+                }
+            }
         }
 
         private void InitializeAjaxFormDictionary(NameValueCollection ajaxForm)
@@ -79,7 +149,7 @@
 
         private Dictionary<string, IEnumerable<FilterModel>> GetCustomFilters()
         {
-            const string PATTERN = @"^custom\[filters\]\[(.+)\]\[(gte|gt|lte|lt)\]$";
+            const string PATTERN = CustomFiltersPatternFormKey;
             var filters = new Dictionary<string, IEnumerable<FilterModel>>();
             foreach (var key in this.ajaxFormDictionary.Keys)
             {
@@ -104,7 +174,7 @@
 
         private List<Column> GetColumns()
         {
-            const string Pattern = @"^columns\[(\d+)\]\[data\]$";
+            const string Pattern = ColumnsFormKeyPattern;
             var columns = new List<Column>();
             var colData = new SortedList<int, string>();
             foreach (var key in this.ajaxFormDictionary.Keys)
@@ -134,14 +204,14 @@
         private Search GetSearch(int key)
         {
             string searchValue = null;
-            if (!this.ajaxFormDictionary.TryGetValue($"columns[{key}][search][value]", out searchValue))
+            if (!this.ajaxFormDictionary.TryGetValue(string.Format(ColumnsSearchValueFormKeyPattern, key), out searchValue))
             {
                 return null;
             }
 
             string isRegexStr = string.Empty;
             bool isRegex = false;
-            if (this.ajaxFormDictionary.TryGetValue($"columns[{key}][search][regex]", out isRegexStr))
+            if (this.ajaxFormDictionary.TryGetValue(string.Format(ColumnsSearchRegexFormKeyPattern, key), out isRegexStr))
             {
                 isRegex = bool.Parse(isRegexStr);
             }
@@ -158,7 +228,7 @@
         private bool IsOrderable(int key)
         {
             string isOrderableStr;
-            if (!this.ajaxFormDictionary.TryGetValue($"columns[{key}][orderable]", out isOrderableStr))
+            if (!this.ajaxFormDictionary.TryGetValue(string.Format(ColumnsOrderableFormKey, key), out isOrderableStr))
             {
                 return false;
             }
@@ -171,7 +241,7 @@
         private bool IsSearchable(int key)
         {
             string isSearchableStr;
-            if (!this.ajaxFormDictionary.TryGetValue($"columns[{key}][searchable]", out isSearchableStr))
+            if (!this.ajaxFormDictionary.TryGetValue(string.Format(ColumnsSearchableFormKey, key), out isSearchableStr))
             {
                 return false;
             }
@@ -184,7 +254,7 @@
         private List<Order> GetOrderList()
         {
             var orders = new List<Order>();
-            const string DirectionPattern = @"^order\[(\d+)\]\[dir\]$";
+            const string DirectionPattern = OrderDirectionFormKeyPattern;
             var colNumbers = new List<int>();
             foreach (var key in this.ajaxFormDictionary.Keys)
             {
@@ -201,8 +271,6 @@
                     });
                 }
             }
-
-            var col = this.ajaxFormDictionary["order[0][column]"];
 
             return orders;
         }
