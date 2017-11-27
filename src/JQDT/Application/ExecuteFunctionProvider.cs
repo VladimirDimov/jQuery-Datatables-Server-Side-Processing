d98f023becfaef647336d3087a8611cd27434e2d
vladko_sz <vdimov1124@gmail.com>
2017-11-27 10:04:17 +0200

    Execute function provider refactpred to generic

--- src/JQDT/Application/ExecuteFunctionProvider.cs
+++ src/JQDT/Application/ExecuteFunctionProvider.cs
@@ -4,31 +4,29 @@
     using System.Collections.Concurrent;
     using System.Linq;
     using System.Linq.Expressions;
-    using System.Web.Mvc;
 
     /// <summary>
     /// Creates and caches application execute function.
     /// </summary>
-    internal static class ExecuteFunctionProvider
+    internal static class ExecuteFunctionProvider<TContext>
     {
-        private static ConcurrentDictionary<Type, Func<ActionExecutedContext, DI.IDependencyResolver, object>> executionFunctionsCache = new ConcurrentDictionary<Type, Func<ActionExecutedContext, DI.IDependencyResolver, object>>();
+        private static ConcurrentDictionary<Type, Func<TContext, DI.IDependencyResolver, object>> executionFunctionsCache = new ConcurrentDictionary<Type, Func<TContext, DI.IDependencyResolver, object>>();
 
         /// <summary>
         /// Gets the execute function.
         /// </summary>
         /// <param name="modelType">Type of the model.</param>
-        /// <param name="appType">Type of the application.</param>
         /// <returns>Application execute function.</returns>
-        internal static Func<ActionExecutedContext, DI.IDependencyResolver, object> GetExecuteFunction(Type modelType, Type appType)
+        internal static Func<TContext, DI.IDependencyResolver, object> GetExecuteFunction(Type modelType, Type appType)
         {
-            Func<ActionExecutedContext, DI.IDependencyResolver, object> executeFunc = null;
+            Func<TContext, DI.IDependencyResolver, object> executeFunc = null;
 
             if (!executionFunctionsCache.TryGetValue(modelType, out executeFunc))
             {
                 Type[] typeArgs = { modelType.GenericTypeArguments.First() };
                 var genericAppType = appType.MakeGenericType(typeArgs);
 
-                var contextExpr = Expression.Parameter(typeof(ActionExecutedContext), "context");
+                var contextExpr = Expression.Parameter(typeof(TContext), "context");
                 var dependencyResolverExpr = Expression.Parameter(typeof(DI.IDependencyResolver));
                 var appConstructorInfo = genericAppType.GetConstructors().First();
                 var newAppExpr = Expression.New(appConstructorInfo, contextExpr, dependencyResolverExpr);
@@ -36,7 +34,7 @@
                 var executeCallExpr = Expression.Call(newAppExpr, executeMethodInfo);
                 var lambda = Expression.Lambda(executeCallExpr, contextExpr, dependencyResolverExpr);
 
-                executeFunc = (Func<ActionExecutedContext, DI.IDependencyResolver, object>)lambda.Compile();
+                executeFunc = (Func<TContext, DI.IDependencyResolver, object>)lambda.Compile();
                 executionFunctionsCache.TryAdd(modelType, executeFunc);
             }
 
--- src/JQDT/JQDataTableAttribute.cs
+++ src/JQDT/JQDataTableAttribute.cs
@@ -32,7 +32,7 @@
         private void PerformOnActionExecuted(ActionExecutedContext filterContext)
         {
             var modelType = filterContext.Controller.ViewData.Model.GetType();
-            var applicationExecuteFunction = ExecuteFunctionProvider.GetExecuteFunction(modelType, typeof(ApplicationMvc<>));
+            var applicationExecuteFunction = ExecuteFunctionProvider<ActionExecutedContext>.GetExecuteFunction(modelType, typeof(ApplicationMvc<>));
             var dependencyResolver = new DI.DependencyResolver();
             var result = (ResultModel)applicationExecuteFunction(filterContext, dependencyResolver);
 
