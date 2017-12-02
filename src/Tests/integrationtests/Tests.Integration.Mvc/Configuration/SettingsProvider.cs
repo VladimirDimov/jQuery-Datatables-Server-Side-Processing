45d0eb4318819a883c3fd28940bc5b549fd3bcba
vladko_sz <vdimov1124@gmail.com>
2017-12-02 17:49:42 +0200

    Integratoin tests cdn urls extracted to web cpnfig

---
+++ src/Tests/integrationtests/Tests.Integration.Mvc/Configuration/SettingsProvider.cs
@@ -0,0 +1,12 @@
+﻿namespace Tests.Integration.Mvc.Configuration
+{
+    using System.Configuration;
+
+    public static class SettingsProvider
+    {
+        public static string Get(string key)
+        {
+            return ConfigurationManager.AppSettings[key];
+        }
+    }
+}
