ab1ea75e38f957ad500cb45fc41760abec2e86d1
vladko_sz <vdimov1124@gmail.com>
2017-12-05 12:49:07 +0200

    get full data from test mvc cached

--- src/Tests/integrationtests/Tests.SeleniumTests/Common/Data.cs
+++ src/Tests/integrationtests/Tests.SeleniumTests/Common/Data.cs
@@ -1,15 +1,25 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
 
 namespace Tests.SeleniumTests.Common
 {
     public static class Data
     {
+        private static Dictionary<Type, object> cachedData = new Dictionary<Type, object>();
+
         public static T GetDataFromServer<T>(string url)
         {
+            if (cachedData.ContainsKey(typeof(T)))
+            {
+                return (T)cachedData[typeof(T)];
+            }
+
             var http = new HttpClient();
             var result = http.GetAsync(url).Result;
             var jsonString = result.Content.ReadAsStringAsync().Result;
             var json = Newtonsoft.Json.JsonConvert.DeserializeObject<T>(jsonString);
+            cachedData.Add(typeof(T), json);
 
             return json;
         }
---
+++ src/Tests/integrationtests/Tests.SeleniumTests/Common/ExceptionsHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace Tests.SeleniumTests.Common
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using OpenQA.Selenium;
+
+    public static class ExceptionsHandler
+    {
+        public static void Hande(Action action, IWebDriver driver)
+        {
+            try
+            {
+                action();
+            }
+            catch (UnhandledAlertException ex)
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                Console.WriteLine(alert.Text);
+                DriverSingletonProvider.Dispose();
+                driver = DriverSingletonProvider.GetDriver();
+                Assert.Fail(ex.Message + "; " + alert.Text);
+            }
+        }
+    }
+}
